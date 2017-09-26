51435adbdf8ef3c98164f467d9ab2896c8c0f3ec
Dev-Osmium <oatmas64134@gmail.com>
2017-09-26 06:41:13 -0400

    Make it work

---
+++ src/main/java/net/devosmium/newjavabot/Main.java
@@ -0,0 +1,20 @@
+package net.devosmium.newjavabot;
+
+import sx.blah.discord.api.IDiscordClient;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        if(args.length != 1){
+            System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
+            return;
+        }
+
+        IDiscordClient cli = BotUtils.getBuiltDiscordClient(args[0]);
+
+        cli.getDispatcher().registerListener(new onMessageReceived());
+        cli.login();
+    }
+
+}
